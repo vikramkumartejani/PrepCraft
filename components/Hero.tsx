29de106284f683a1f170715722c231576908c01b
vikram kumar <vikramkumar8t8@gmail.com>
2024-12-05 21:41:05 +0500

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,18 +6,22 @@ import OrbitingCirclesDemo from "./OrbitingCirclesDemo";
 const Hero = () => {
   return (
     <div
-      className="pt-[20px] lg:pt-[36px] bg-[#071125] relative w-full md:h-[1100px] sm:h-[800px] h-[500px]  overflow-hidden"
-      style={{ backgroundImage: 'url("/assets/hero-bg.png")' }}
+      className="pt-[20px] lg:pt-[36px]  relative w-full md:h-[1100px] sm:h-[800px] h-[500px] overflow-hidden"
+      
     >
+      <div className="bg-[#071125]  absolute inset-0 w-full h-full"></div>
+      <img src='/assets/hero-bg-colors.png' alt='Hero Background Colors' className='opacity-20 absolute inset-0 w-full h-full object-cover' />
+      <img src='/assets/online-platform-where-robots-assist-language-learning-through-conversation.png' alt='Hero Background Colors' className='opacity-20 absolute inset-0 w-full h-full object-cover' />
       <Image
         src="/assets/cube.png"
         alt="cube"
         width={200.72}
         height={186.07}
-        className="absolute top-[142px] xl:left-[80px] lg:left-[20px] left-[10px] z-0"
+        className="absolute top-[142px] xl:left-[80px] lg:left-[20px] left-[10px] z-0 "
       />
       <Navbar />
 
+      {/* style={{ backgroundImage: 'url("/assets/")' }} */}
       <div className="mt-10 relative z-30 max-w-[1180px] w-full mx-auto h-full">
         <OrbitingCirclesDemo />
       </div>
