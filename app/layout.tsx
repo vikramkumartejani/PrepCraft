1f5ee5666693a5a3cc0f4e7a88f60cf7ab1108fb
vikram kumar <vikramkumar8t8@gmail.com>
2024-12-05 17:51:15 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "PrepCraft",
-  description: "PrepCraft",
+  description: "PrepCraf",
 };
 
 export default function RootLayout({
